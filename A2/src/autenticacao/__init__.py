7055fd0345a3260140f8399642257c942505b223
Giovana Jacome Marchetti <g177700@dac.unicamp.br>
2026-09-17 14:20:32 -0300

    chore: acrescenta importações para os outros escopos

--- A2/src/autenticacao/__init__.py
+++ A2/src/autenticacao/__init__.py
@@ -1,2 +1,4 @@
 from .base import Usuario, ServicoAutenticacao
 from .australia import EleitorAustralia, CandidatoAustralia
+from .caco import EstudanteCACo, GestaoCACo
+from .botc import JogadorBOTC, StoryTellerBOTC
