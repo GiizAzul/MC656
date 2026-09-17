f0b083887ebc55ded929327fe99bab3b78e2e7a3
Giovana Jacome Marchetti <g177700@dac.unicamp.br>
2026-09-17 09:10:53 -0300

    feat: Adiciona biblioteca para método abstrato escopo que identifica qual o contexto e cria método de validação simples

--- A2/src/autenticacao.py
+++ A2/src/autenticacao.py
@@ -1,7 +1,7 @@
+from abc import ABC, abstractmethod
 
 
-
-class Usuario():
+class Usuario(ABC):
     """Classe base abstrata para todos os usuários do sistema."""
 
     def __init__(self, id: int, username:str, senha: str, nome_real: str):
@@ -10,4 +10,11 @@ class Usuario():
         self.senha = senha
         self.nome_real = nome_real
 
-    
+    @property
+    @abstractmethod
+    def escopo(self) -> str:
+        """Identificar qual o contexto entre as possíveis eleições"""
+        pass
+
+    def validar_senha(self, tentativa: str) -> bool:
+        return self.senha == tentativa
