b536c6a9fa5be62342458ae2da1707ad7679eecd
Giovana Jacome Marchetti <g177700@dac.unicamp.br>
2026-09-17 19:36:06 -0300

    fix: Conserta problema de imports nunca usados no ruff

--- A2/src/autenticacao/__init__.py
+++ A2/src/autenticacao/__init__.py
@@ -1,4 +1,15 @@
-from .australia import CandidatoAustalia, EleitorAustralia
+from .australia import CandidatoAustralia, EleitorAustralia
 from .base import ServicoAutenticacao, Usuario
 from .botc import JogadorBOTC, StoryTellerBOTC
 from .caco import EstudanteCACo, GestaoCACo
+
+__all__ = [
+    "CandidatoAustralia",
+    "EleitorAustralia",
+    "EstudanteCACo",
+    "GestaoCACo",
+    "JogadorBOTC",
+    "ServicoAutenticacao",
+    "StoryTellerBOTC",
+    "Usuario"
+]
