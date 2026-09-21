52273fb637b61300c245f91b8fb6360f22b475a9
Leonardo Carvalho De Luca <r288820@dac.unicamp.br>
2026-09-21 17:40:46 -0300

    [test] muda nome nos testes

--- A2/tests/test_botc.py
+++ A2/tests/test_botc.py
@@ -5,25 +5,25 @@ from src.botc import SistemaEleitoralBotC
 
 def test_votos_insuficientes():
     sistema = SistemaEleitoralBotC(10)
-    sistema.registrar_votacao("Alice", 4)
+    sistema.registrar_votacao("Gi", 4)
     assert sistema.apurar_vencedor() is None
 
 def test_votos_suficientes_sucesso():
     sistema = SistemaEleitoralBotC(10)
-    sistema.registrar_votacao("Alice", 5)
-    assert sistema.apurar_vencedor() == "Alice"
+    sistema.registrar_votacao("Gi", 5)
+    assert sistema.apurar_vencedor() == "Gi"
 
 def test_empate_na_maioria():
     sistema = SistemaEleitoralBotC(10)
-    sistema.registrar_votacao("Alice", 6)
-    sistema.registrar_votacao("Bob", 6)
+    sistema.registrar_votacao("Gi", 6)
+    sistema.registrar_votacao("Caio Lima", 6)
     assert sistema.apurar_vencedor() is None
 
 def test_maior_voto_vence():
     sistema = SistemaEleitoralBotC(10)
-    sistema.registrar_votacao("Alice", 6)
-    sistema.registrar_votacao("Bob", 8)
-    assert sistema.apurar_vencedor() == "Bob"
+    sistema.registrar_votacao("Gi", 6)
+    sistema.registrar_votacao("Caio Lima", 8)
+    assert sistema.apurar_vencedor() == "Caio Lima"
 
 def test_numero_vivos_invalido():
     with pytest.raises(ValueError, match="O número de jogadores vivos deve ser maior que zero."):
@@ -32,12 +32,12 @@ def test_numero_vivos_invalido():
 def test_votos_negativos_invalido():
     sistema = SistemaEleitoralBotC(10)
     with pytest.raises(ValueError, match="O número de votos não pode ser negativo."):
-        sistema.registrar_votacao("Alice", -1)
+        sistema.registrar_votacao("Gi", -1)
 
 def test_votos_maior_numero_vivos_invalido():
     sistema = SistemaEleitoralBotC(10)
     with pytest.raises(ValueError, match="O número de votos não pode ser maior que a quantidade de jogadores."):
-        sistema.registrar_votacao("Alice", 11)
+        sistema.registrar_votacao("Gi", 11)
 
 def test_sem_votos():
     sistema = SistemaEleitoralBotC(10)
